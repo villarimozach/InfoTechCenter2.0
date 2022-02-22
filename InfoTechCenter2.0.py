45e41ce5c5c587350ece6342a04a87c753c900b2
Zachary Villarimo <02zachary.villarimo@gulllakecs.org>
2022-02-22 13:43:10 -0500

    Completed gasLevelAlert Function and is stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -6,11 +6,25 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     #print(currentGasLevel)
     return currentGasLevel
+# Variable calls the value of gasLevelGauge Function
+gasLevelIndicator = gasLevelGauge()
 
-# Create If-ELIF-ELSE statements using comparative Operators to display gas level messages
+# Create If-ELIF-ELSE statements using the comparative Operator == Equal to in order to display gas level messages to display gas level messages
 def gasLevelAlert():
-    if gasLevelGauge() == "Empty":
-        print("***WARNING***\n You have run out of gas\n Calling Emergency Contact")
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOU ARE ON EMPTY***\nCalling Emergency Contact")
+    elif gasLevelIndicator == "Low":
+        print("***WARNING***\nYour gas tank is extremely LOW\nThe closest gasoline station is")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("You are on a QUARTER TANK of gas\nStart planing a visit to the gas station!!!")
+    elif gasLevelIndicator == "Haft Tank":
+        print("You are on a HAFT TANK of gas\nYou have 125 more miles to empty!!!")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("You are on a THREE QUARTER TANK of gas\nYou have 205 more miles to empty!!!")
+    else:
+        print("You have a FULL TANK of gas, Congratulations.\nYou have 310 more miles to empty!!!")
 
+
+ # Call Functions Here
 gasLevelGauge()
 gasLevelAlert()
