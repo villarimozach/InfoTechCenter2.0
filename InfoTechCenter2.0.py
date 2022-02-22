4019ad1753e4082db0399a40cf3432bf59745838
Zachary Villarimo <02zachary.villarimo@gulllakecs.org>
2022-02-22 14:07:02 -0500

    Added sleep after second print statment

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -3,3 +3,37 @@ from time import sleep #Print to one line with time delay between prints
 print("\033[1;34m Welcome To InfoTechCenter  \n")
 sleep(1)
 print("\033[1;34m Hornet's Operating System Booting Up  \n")
+
+import random
+
+# Gas Level Function
+sleep(1)
+def gasLevelGauge():
+    gasLevelList = ["Empty","Low","Quarter Tank","Haft Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    #print(currentGasLevel)
+    return currentGasLevel
+# Variable calls the value of gasLevelGauge Function
+gasLevelIndicator = gasLevelGauge()
+
+# Create If-ELIF-ELSE statements using the comparative Operator == Equal to in order to display gas level messages to display gas level messages
+def gasLevelAlert():
+    gasStations = ["Shell","BP","Citgo","Circle K","Mobil","Speedway","Marathon","Love's","Meijer","Costco","Sunoco"]
+    miles = round(random.uniform(1,25), 1)
+    if gasLevelIndicator == "Empty":
+        print("***WARNING***\nYOU ARE ON EMPTY!!!\nCalling Emergency Contact")
+    elif gasLevelIndicator == "Low":
+        print("***WARNING***\nYour gas tank is extremely LOW\nThe closest gasoline station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away!!!")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("You are on a QUARTER TANK of gas\nStart planing a visit to the gas station!!!")
+    elif gasLevelIndicator == "Haft Tank":
+        print("You are on a HAFT TANK of gas\nYou have 125 more miles to empty!!!")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("You are on a THREE QUARTER TANK of gas\nYou have 205 more miles to empty!!!")
+    else:
+        print("You have a FULL TANK of gas, Congratulations.\nYou have 310 more miles to empty!!!")
+
+
+ # Call Functions Here
+gasLevelGauge()
+gasLevelAlert()
