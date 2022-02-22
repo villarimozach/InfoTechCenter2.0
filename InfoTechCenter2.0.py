81854078ec6522129021e8b58d6fd7c1e83b9416
Zachary Villarimo <02zachary.villarimo@gulllakecs.org>
2022-02-22 13:45:33 -0500

    Changed Miles Variable from an Int to a Float

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -12,7 +12,7 @@ gasLevelIndicator = gasLevelGauge()
 # Create If-ELIF-ELSE statements using the comparative Operator == Equal to in order to display gas level messages to display gas level messages
 def gasLevelAlert():
     gasStations = ["Shell","BP","Citgo","Circle K","Mobil","Speedway","Marathon","Love's","Meijer","Costco","Sunoco"]
-    miles = random.randint(1,25)
+    miles = round(random.uniform(1,25), 1)
     if gasLevelIndicator == "Empty":
         print("***WARNING***\nYOU ARE ON EMPTY!!!\nCalling Emergency Contact")
     elif gasLevelIndicator == "Low":
